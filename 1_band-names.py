1ff6d5a93b8edd699ab00b3589505229d05e4024
Siarhei Novik <Siarhei.Novik@ao.com>
2024-04-05 23:26:56 +0200

    Add band name generator script

---
+++ 1_band-names.py
@@ -0,0 +1,10 @@
+#ask user for town where they grew up
+town = input("Where did you grow up? ")
+
+#ask user for name of a pet
+pet = input("What is the name of your pet? ")
+
+#combine the name of their town and pet and show them their band name
+
+print("Your band name could be " + town + " " + pet)
+
