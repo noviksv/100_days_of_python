33b94e8e9a8d554939d0ac3000908800dfc8d518
noviksv <novik.s@tut.by>
2024-04-09 22:40:43 +0200

    Refactor main.py to include menu selection and payment functionality

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,27 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+
+if __name__ =='__main__':
+    coffee_machine = CoffeeMaker()
+    #coffee_machine.report() 
+    menu = Menu()
+    money_machine = MoneyMachine()
+    while True:
+        print(f"What would you like? ({menu.get_items()})")
+        input_order = str(input())
+        if input_order == 'report':
+            coffee_machine.report()
+            money_machine.report()
+        elif input_order == 'off':
+            break
+        else:
+            order = menu.find_drink(input_order)
+            if order:
+                if coffee_machine.is_resource_sufficient(order):
+                    if money_machine.make_payment(order.cost):
+                        coffee_machine.make_coffee(order)
+
