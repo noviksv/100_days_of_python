2726b529de8f38cde1b990dffb6dc556ed3c8d32
noviksv <novik.s@tut.by>
2024-04-11 22:53:04 +0200

    Add trivia data to quiz game

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,13 +1,15 @@
 from question_model import Question
 from data import question_data
+from data_trivia import data as data_trivia
 from quiz_brain import QuizBrain
+
 
 # add type ann anotations to the question_bank list
 question_bank = []
 
-for question in question_data:
-    text = question["text"]
-    answer = question["answer"]
+for question in data_trivia:
+    text = question["question"]
+    answer = question["correct_answer"]
     new_question = Question(text, answer)
     question_bank.append(new_question)
 
