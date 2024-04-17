1f95e2bc1f5d34aeb0938bd540c10044b88472d1
noviksv <novik.s@tut.by>
2024-04-17 10:49:38 +0200

    Add Pong Game functionality using turtle module

---
+++ 22-pong-game/main.py
@@ -0,0 +1,12 @@
+from turtle import Screen, exitonclick
+import time
+
+# Screen setup
+screen = Screen()
+screen.setup(width=800, height=600,startx=300, starty=300)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+
+exitonclick()
