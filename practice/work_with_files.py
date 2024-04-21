720cdeff40da28dcae09773aae7c1ad905d99185
noviksv <novik.s@tut.by>
2024-04-21 15:14:36 +0200

    Add sum of numbers to file.tmp

--- practice/work_with_files.py
+++ practice/work_with_files.py
@@ -8,3 +8,10 @@ with open("file.tmp", "w") as file:
         file.write(f"{random.randint(1, 100)}\n")
 # Read the file and print the sum of the numbers in the file
 
+# appending the sum of the numbers to the file
+with open("file.tmp", "r") as file:
+    numbers = file.readlines()
+    sum_of_numbers = sum([int(number) for number in numbers])
+    print(sum_of_numbers)
+    with open("file.tmp", "a") as file:
+        file.write(f"Sum of the numbers: {sum_of_numbers}")
