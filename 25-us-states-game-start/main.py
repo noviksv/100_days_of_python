65d526445c31cc0a5d4f201ebb3d8a2d2e41dd98
noviksv <novik.s@tut.by>
2024-04-25 09:23:40 +0200

    Refactor main.py to save unguessed states in a separate file

--- 25-us-states-game-start/main.py
+++ 25-us-states-game-start/main.py
@@ -12,15 +12,22 @@ game_is_on = True
 score = 0 
 
 data = pd.read_csv("50_states.csv")
-data.state = data.state.str.lower()
 correct_states = []
 
+
+def save_data():
+    #save unguessed states
+    unguessed_states = [i for i in data.state.to_list() if i not in correct_states]
+    new_data = pd.DataFrame(unguessed_states)
+    new_data.to_csv("unguessed_states.csv")
+
 while game_is_on:  
-    answer_state = screen.textinput(f"{score}/50 What is the state's name?", "What's another state's name?").title().lower()
-    if answer_state == "exit":
+    answer_state = screen.textinput(f"{score}/50 What is the state's name?", "What's another state's name?").title()
+    if answer_state == "Exit":
+        save_data()
         break
     if answer_state in data.state.to_list() and  answer_state not in correct_states:
-        print(data.state.to_list())
+        #print(data.state.to_list())
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
@@ -40,5 +47,5 @@ while game_is_on:
 
 
 
-screen.exitonclick()
+#screen.exitonclick()
 
