ed1f954cf041d2bc53132974ecafaef71d9b50af
noviksv <novik.s@tut.by>
2024-05-05 19:02:57 +0200

    Add GUI functionality to miles-to-km-converter

--- practice/gui.py
+++ practice/gui.py
@@ -2,24 +2,34 @@ import tkinter
 
 def button_click():
     print("Button was clicked!")
+    #change the text of the label
+    label.config(text=input_field.get())
 
-def main():
-    root = tkinter.Tk()
-    root.title("Hello, World!")
-    #define the size of the window
-    root.geometry("800x600")
-    #create a label widget
-    label = tkinter.Label(root, text="Hello, World!", font=("Arial", 24, "bold"))
-    #pack the label widget
-    label.pack(side="left")
 
-    #create a button widget
-    button = tkinter.Button(root, text="Click me!", font=("Arial", 24, "bold"))
-    #pack the button widget
-    button.pack(side="right")
-    #bind the button to a function
-    button.config(command=button_click)
-    root.mainloop()
 
-if __name__ == "__main__":
-    main()
+root = tkinter.Tk()
+root.title("Hello, World!")
+#define the size of the window
+root.geometry("800x600")
+#create a label widget
+label = tkinter.Label(root, text="Hello, World!", font=("Arial", 24, "bold"))
+#pack the label widget
+label.pack(side="left")
+
+#add input field with default text
+
+input_field = tkinter.Entry(root, width=10)
+input_field.insert(0, 'Default')
+input_field.pack()
+print(input_field.get())
+
+
+#create a button widget
+button = tkinter.Button(root, text="Click me!", font=("Arial", 24, "bold"))
+button.pack(side="right")
+#bind the button to a function
+button.config(command=button_click)
+
+
+
+root.mainloop()
