d11ddfd46eeee08fe58f632b5e57fbc4e7bd5db4
noviksv <novik.s@tut.by>
2024-05-17 23:25:03 +0200

    feat: Add logo to Password Manager UI

--- 29-password-manager/main.py
+++ 29-password-manager/main.py
@@ -3,3 +3,25 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+from tkinter import *
+YELLOW = "#f7f5dd"
+# add logo to screen
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+#add a canvas
+canvas = Canvas(width=200, height=200,  highlightthickness=0)
+tomato_img = PhotoImage(file="logo.png")
+img = canvas.create_image(100, 100, image=tomato_img)
+canvas.create_image(100, 100, image=tomato_img)
+canvas.grid()
+
+
+
+
+window.mainloop()
+
