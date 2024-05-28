0c1de539e032f06b472f30068594936668f9aaa5
noviksv <novik.s@tut.by>
2024-05-28 08:57:45 +0200

    Update Password Manager UI with logo and form layout

--- 29-password-manager/main.py
+++ 29-password-manager/main.py
@@ -13,13 +13,29 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-#add a canvas
+#add a canvas 3x5 grid
 canvas = Canvas(width=200, height=200,  highlightthickness=0)
-tomato_img = PhotoImage(file="logo.png")
-img = canvas.create_image(100, 100, image=tomato_img)
-canvas.create_image(100, 100, image=tomato_img)
-canvas.grid()
+logo_img = PhotoImage(file="logo.png")
+img = canvas.create_image(100, 100, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
+label_website = Label(text="Website:")
+label_website.grid(row=1, column=0)
+entry_website = Entry(width=35)
+entry_website.grid(row=1, column=1, columnspan=2)
+label_email = Label(text="Email/Username:")
+label_email.grid(row=2, column=0)
+entry_email = Entry(width=35)
+entry_email.grid(row=2, column=1, columnspan=2)
+label_password = Label(text="Password:")
+label_password.grid(row=3, column=0)
+entry_password = Entry(width=21)
+entry_password.grid(row=3, column=1)
+button_generate_password = Button(text="Generate Pass", width=10)
+button_generate_password.grid(row=3, column=2)
+button_add = Button(text="Add", width=36)
+button_add.grid(row=4, column=1, columnspan=2)
 
 
 
