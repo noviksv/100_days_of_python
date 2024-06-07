cca1c1864ea4f2bc2a7a5b34899c94d8a4b45351
noviksv <novik.s@tut.by>
2024-06-07 17:42:29 +0200

    chore: Add functionality to send random quotes on Fridays via email

--- 32-birthday-wisher/main.py
+++ 32-birthday-wisher/main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import os
 import smtplib
 import logging
+import random
+import datetime as dt
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,12 +17,31 @@ SMTP_LOGIN = os.getenv("SMTP_LOGIN")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 SMTP_FROM = os.getenv("SMTP_FROM")
 
-with smtplib.SMTP(SMTP_HOST) as connection:
-    connection.starttls()
-    connection.login(user=SMTP_LOGIN, password=SMTP_PASSWORD)
-    connection.sendmail(
-        from_addr=SMTP_FROM,
-        to_addrs=SMTP_FROM,
-        msg="Subject:Hello\n\nThis is the body of my email"
-    )
-    logging.info("Email sent")
+def send_email(message_body):
+    with smtplib.SMTP(SMTP_HOST) as connection:
+        connection.starttls()
+        connection.login(user=SMTP_LOGIN, password=SMTP_PASSWORD)
+        connection.sendmail(
+            from_addr=SMTP_FROM,
+            to_addrs=SMTP_FROM,
+            msg=f"Subject:Hello\n\n{message_body}"
+        )
+        logging.info("Email sent")
+
+def get_random_quote():
+    with open("quotes.txt") as file:
+        quotes = file.readlines()
+    quote = random.choice(quotes)
+    logging.info(f"Selected quote: {quote}")
+    return quote
+
+def main():
+    if dt.datetime.now().weekday() == 4:
+        logging.info("It's Friday! Sending email.")
+        send_email(get_random_quote())
+    else:
+        logging.info("It's not Friday. No email sent.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ practice/dt.py
@@ -0,0 +1,9 @@
+import datetime as dt
+
+now = dt.datetime.now()
+print(now)
+print(now.month, now.year)
+
+date_of_birth = dt.datetime(year=1990, month=1, day=1)
+
+print(date_of_birth)
