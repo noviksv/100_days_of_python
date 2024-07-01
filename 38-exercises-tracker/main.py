4506a9b97015b89ce682d1335fc160294434cf44
noviksv <novik.s@tut.by>
2024-07-01 17:18:53 +0200

    add basic auth to sheety

--- 38-exercises-tracker/main.py
+++ 38-exercises-tracker/main.py
@@ -5,6 +5,22 @@ import datetime
 
 load_dotenv()
 
+def add_records_to_sheet(workout_params):
+    # 
+    # "date": "30/06/2024",
+    # "time": "15:18:32",
+    # "exercise": "running",
+    # "duration": "35",            "calories": "200" }  
+
+    sheety_endpoint  =  os.getenv("SHEETY_ENDPOINT")
+    headers = {
+        "Authorization": f"{os.getenv('SHEETY_BASIC_AUTH')}"
+    }
+    sheety_params = {"workout":
+                     workout_params }
+    r = requests.post(url=sheety_endpoint, headers=headers, json=sheety_params)
+    r.raise_for_status()
+    pass
 
 def process_exercises(query):
 
@@ -15,12 +31,9 @@ def process_exercises(query):
     }
 
     host = "https://trackapi.nutritionix.com"
-    # endpoint = "/v2/natural/nutrients"
-    # r = requests.post(url=f"{host}{endpoint}",headers=headers, params={"query":"1 large apple"})
+    # endpoint = "/v2/search/instant"
 
-
-    endpoint = "/v2/search/instant"
-    r = requests.get(url=f"{host}{endpoint}",headers=headers, params={"query":"apple"})
+    # r = requests.get(url=f"{host}{endpoint}",headers=headers, params={"query":"apple"})
 
 
     # process exercises
@@ -36,10 +49,23 @@ def process_exercises(query):
 
     r.raise_for_status()
     data = r.json()
+    a = []
     for exercise in data["exercises"]:
-        print(exercise["name"], exercise["duration_min"], exercise["nf_calories"])
+        a.append({"exercise" : exercise["name"]
+                  , "duration": exercise["duration_min"]
+                  , "calories" : exercise["nf_calories"]}
+                )
+        
+    return a
 
 
 if __name__ == "__main__":
     exercise_input  = input("Tell me which exercises you did: ")
     process_exercises(exercise_input)   
+    today = datetime.datetime.now()
+    date = today.strftime("%d/%m/%Y")
+    time = today.strftime("%H:%M:%S")
+    dttm_dict = {"date": date, "time": time}
+    for result in process_exercises("apple"):
+        add_records_to_sheet (workout_params={ **dttm_dict,
+                                               **result } )
